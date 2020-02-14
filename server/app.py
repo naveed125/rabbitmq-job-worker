7cf61c22ba609761ea8a62ea60bf56abcff28853
Naveed Khan <naveed@gmail.com>
2020-02-13 23:22:53 -0800

    working system

--- server/app.py
+++ server/app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+import pika
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return 'OK'
+
+
+@app.route('/add-job/<cmd>')
+def add(cmd):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=cmd,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+    return " [x] Sent: %s" % cmd
 
 
 if __name__ == '__main__':
--- worker/app.py
+++ worker/app.py
@@ -1,21 +1,34 @@
 import pika
 import time
 
+sleepTime = 10
+print(' [*] Sleeping for ', sleepTime, ' seconds.')
+time.sleep(30)
+
+print(' [*] Connecting to server ...')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
-
 channel.queue_declare(queue='task_queue', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+
+print(' [*] Waiting for messages.')
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    time.sleep(body.count(b'.'))
+    print(" [x] Received %s" % body)
+    cmd = body.decode()
+
+    if cmd == 'hey':
+        print("hey there")
+    elif cmd == 'hello':
+        print("well hello there")
+    else:
+        print("sorry i did not understand ", body)
+
     print(" [x] Done")
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='task_queue', on_message_callback=callback)
-
 channel.start_consuming()
